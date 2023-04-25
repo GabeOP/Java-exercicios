e5c03270281164c6142c1cb53aa2eec773d7383d
Gabriel <gabrieloliveirap1@hotmail.com>
2023-04-25 14:06:28 -0300

    exercicio-fixacao-java

---
+++ exercicio-pedido/src/entities/OrderItem.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class OrderItem {
+    private Integer quantity;
+    private Double price;
+    private Product product;
+    public OrderItem(Integer quantity, Double price, Product product){
+        this.quantity = quantity;
+        this.price = price;
+        this.product = product;
+    }
+    public Double subTotal(){
+        return price * quantity;
+    }
+}
